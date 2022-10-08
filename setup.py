c72febaff004afa186eccc2aee6745d5f45df125
Mervin Hemaraju <th3pl4gu33@gmail.com>
2022-10-08 18:39:03 +0400

    - Updated setup.py

--- setup.py
+++ setup.py
@@ -5,13 +5,13 @@ with open("README.md", "r", encoding="utf-8") as rm:
 
 setup(
     name="freshtasks",
-    version="1.0",
+    version="1.1",
     author="Mervin Hemaraju",
-    author_email="mervin.hemaraju@checkout.com",
+    author_email="th3pl4gu33@gmail.com",
     description="Fresh Task is a Python package that fetches a list of tasks from Fresh Service tickets.",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/mervin-hemaraju-cko/fresh-tasks",
+    url="https://github.com/mervinhemaraju/freshtasks",
     packages=find_packages(exclude=('tests')),
     install_requires=[
           'requests'
